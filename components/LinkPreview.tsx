b335059c48caf38c6b13c13c93c95b10037edf7a
Nayan Chandra Datta <nayanchdatta02@gmail.com>
2023-09-29 11:25:01 +0600

    text color fix

--- components/LinkPreview.tsx
+++ components/LinkPreview.tsx
@@ -14,7 +14,7 @@ const LinkPreview = ({ linkPreview }: LinkPreviewProps) => {
     return (
         <div>
             <h2 className='text-lg font-bold mb-1 mt-6'>{linkPreview.title}</h2>
-            <p className='text-base text-slate-200 mb-3'>{linkPreview.description}</p>
+            <p className='text-base mb-3'>{linkPreview.description}</p>
             {linkPreview.image ?
                 <img
                     src={linkPreview.image}
